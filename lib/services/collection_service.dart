9290814522a48867f692ac0d41ed1585fe9c4167
Jarno Braeuner <jarno.braeuner@gmail.com>
2026-04-20 14:49:31 +0200

    Update

--- lib/services/collection_service.dart
+++ lib/services/collection_service.dart
@@ -128,7 +128,7 @@ class CollectionService extends ChangeNotifier {
     _sets.addAll([
       CollectionSet(
         id: 'set_hamburg',
-        name: 'Hamburg Klassiker',
+        name: 'Hamburg',
         description: 'Sammle alle klassischen SehenswÃ¼rdigkeiten in Hamburg',
         requiredTokenIds: ['1', '2', '3', '4', '5', '6', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'],
         bonusPoints: 800,
@@ -136,7 +136,7 @@ class CollectionService extends ChangeNotifier {
       ),
       CollectionSet(
         id: 'set_dissen',
-        name: 'Dissen Klassiker',
+        name: 'Dissen',
         description: 'Entdecke alle Sehenswürdigkeiten in Dissen',
         requiredTokenIds: ['7', '8', '9', '10', '11', '12'],
         bonusPoints: 500,
@@ -144,7 +144,7 @@ class CollectionService extends ChangeNotifier {
       ),
       CollectionSet(
         id: 'set_leipzig',
-        name: 'Leipzig Klassiker',
+        name: 'Leipzig',
         description: 'Entdecke alle Sehenswürdigkeiten in Leipzig',
         requiredTokenIds: ['52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68'],
         bonusPoints: 700,
